48fe39abc010dc7d1a94203fc411a71e3a1be892
Andy Hoffman <antibland@gmail.com>
2019-02-17 14:42:12 -0800

    Spacing

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
@@ -16,21 +16,22 @@ app.listen(4000, () => {
 });
 
 function verifyToken(req, res, next) {
-	var token = req.body.token || req.query.token || req.headers['x-access-token'];
-	// If token not present, deny access
-	if (!token) return res.json({ success: false, message: "No token provided" })
-	// Attempt token verification
-	jwt.verify(token, 'MY_SECRET', (err, decodedData) => {
-		// if problem with token verification, deny access
+  const token = req.body.token || req.query.token || req.headers['x-access-token'];
+  // If token not present, deny access
+  if (!token) return res.json({ success: false, message: "No token provided" });
+  // Attempt token verification
+  jwt.verify(token, 'MY_SECRET', (err, decodedData) => {
+    // if problem with token verification, deny access
     if (err) return res.json({success: false, message: "Invalid token."})
 
     next();
-	})
+  });
 }
 
 const counterApi = require("./controllers/counter.js");
 const user = require("./controllers/user.js");
 
+// Protect api routes
 app.all('/api/*', verifyToken);
 
 app.post('/login', user.login);
@@ -38,5 +39,5 @@ app.post('/api/counter', counterApi.getCount);
 app.post('/api/counter/increment', counterApi.incrementCount);
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname + "/client/public/index.html"));
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
 });
